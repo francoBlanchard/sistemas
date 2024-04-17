b58984a7931c9c18e05f27b1695eff7a4ba500a6
Franco Blanchard <blanchardfranco@gmail.com>
2024-04-17 16:44:52 -0300

    texto

---
+++ sin título3.pas
@@ -0,0 +1,19 @@
+  program eje_6;
+
+var
+
+begin
+	c := [1 ..50]
+	a:=[];
+	
+	//for i:= 1 to 8 do inclusive (a, 6 + i)
+	
+	for i:= 1 to 8 do 
+	a:= a + [ 6*i];
+		writeln(a, e'; ');
+		b:= c -a;
+		writeln;
+		write('elementos de b: ');
+		for e in b do 
+		write(e, '; ');
+end,
